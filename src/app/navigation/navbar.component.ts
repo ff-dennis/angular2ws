4c2d12ba6d0abff0be3c6e5ab81d3e23a4bbce9e
Christian Hildebrandt <Christian.Scheil.pentasys@gmail.com>
2016-09-04 20:56:10 +0200

    Collapse Navbar in mobile view

--- src/app/navigation/navbar.component.ts
+++ src/app/navigation/navbar.component.ts
@@ -7,9 +7,15 @@ import { Router} from '@angular/router'
 })
 
 export class NavbarComponent {
+	_isCollapsed = true;
+
 	constructor(private _router: Router) { }
 
 	isCurrentRoute(route: string) {
 		return this._router.isActive(route, true);
+	}
+
+	toggleCollapse() {
+		this._isCollapsed = !this._isCollapsed;
 	}
 }
