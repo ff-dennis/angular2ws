bb7f4f7710e5ec4cc55fb5900e580bc7015fb8f8
Christian Hildebrandt <Christian.Scheil.pentasys@gmail.com>
2016-09-04 16:18:15 +0200

    removed angular seed boilerplate; implemented first component navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from './navigation/navbar.component'
 
 @Component({
-  selector   : 'app',
-  templateUrl: './app.html',
+  selector: 'app',
+  template: `
+    <h3>
+      Angular 2 Blog
+    </h3>
+    <navbar></navbar>
+  `,
+  directives: [NavbarComponent]
 })
 export class AppComponent {
 }
--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -4,9 +4,6 @@
 import '@angular/platform-browser-dynamic';
 import '@angular/platform-browser';
 import '@angular/core';
-import '@angular/http';
-import '@angular/router';
-
 
 // RxJS 5
 // import 'rxjs/Rx';
