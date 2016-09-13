6758518b4d81a164208f33d4487c6862352531f3
Christian Hildebrandt <Christian.Scheil.pentasys@gmail.com>
2016-09-13 11:33:28 +0200

    Our Real First Component - a Navigation Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core'
+import {NavbarComponent} from './navbar/navbar.component'
 
 @Component({
     selector: 'app',
-    template: 'Hi it is me - the app!'
-
+    template: `
+    <h3>
+        Angular 2 Blog
+    </h3>
+    <navbar></navbar>
+    `
 })
 export class AppComponent { }
