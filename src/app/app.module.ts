af678d680ad956f7f62a61cf0f0db4fe1f7c37a2
Christian Hildebrandt <Christian.Scheil.pentasys@gmail.com>
2016-09-22 14:31:55 +0200

    Adding Posts to Routing and set active tab class with directive rouerLinkActive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import {AppComponent} from "./app.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {AgendaComponent} from "./agenda/agenda.component";
 import {UsersComponent} from "./users/users.component";
+import {PostsComponent} from './posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, AgendaComponent, UsersComponent],
+  declarations: [AppComponent, NavbarComponent, AgendaComponent, UsersComponent, PostsComponent],
   imports     : [BrowserModule, RouterModule.forRoot(rootRouterConfig)],
   providers   : [],
   bootstrap   : [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import {Routes} from '@angular/router';
 
 import {AgendaComponent} from './agenda/agenda.component';
 import {UsersComponent} from './users/users.component';
+import {PostsComponent} from './posts/posts.component';
 
 export const rootRouterConfig: Routes = [
     { path: 'agenda', component: AgendaComponent },
     { path: 'users', component: UsersComponent },
+    { path: 'posts', component: PostsComponent },
     { path: '**', redirectTo: 'agenda' }]
