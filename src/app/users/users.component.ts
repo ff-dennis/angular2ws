7acb47c4ee3d6bee66d3c0b15106ae3ac2e3bb50
Christian Hildebrandt <Christian.Scheil.pentasys@gmail.com>
2016-09-23 10:48:37 +0200

    Added table to users and a simple users Array in component - show one way binding

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,4 +7,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class UsersComponent {
 
+	users = [];
+
+	constructor(){
+		this.users.push({
+			"name":"Fred",
+			"email":"Fred.Feuerstein@pentasys.de"
+		});
+		this.users.push({
+			"name":"Barney",
+			"email":"Barney.Geröllheimer@pentasys.de"
+		});
+	}
 }
